d46a96595e4a8528b7368dfaacb33f71d0b928db
Yeison Cordoba <yeisoncordoba672@gmail.com>
2023-07-06 15:04:47 -0500

    Actualizacion

--- src/Variables.java
+++ src/Variables.java
@@ -1,6 +1,19 @@
 public class Variables {
     public static void main (String[] args){
-        //Delcaracion de vaariables
+        //Delcaracion de variables
         int speed;
+
+        //Asignacion de una variable
+        speed = 10;
+        System.out.println(speed);
+
+
+        //Delcaracion e inicializacion de tipo entero
+        int salary = 1000;
+
+        //Declaracion e inicializacion de tipo texto
+        String employeeName = "Yeison Cordoba";
+        System.out.println(employeeName);
+
     }
 }
