cd0ec14f16f6342af162df0b2b46caab04147cd3
Yeison Cordoba <yeisoncordoba672@gmail.com>
2023-07-07 13:58:08 -0500

    Actualizacion

---
+++ src/NamingJava.java
@@ -0,0 +1,34 @@
+public class NamingJava {
+    public static void main (String[] args){
+
+        int cellPhone = 31512155;
+        System.out.println(cellPhone);
+
+        //Convencion de nombres
+        String $countryName = "Andres";
+        String _bacgroundColor = "Verde";
+        String corrency$ = "COP";
+        String Background_color = "Rojo";
+
+
+        //Constantes en java  lo escribe en MAYSCULA
+        int POSITION  = -5;
+        int MAX_WIDTH = 9990;
+        int MIN_WIDTH = 1;
+
+        //CAMEL CASE
+
+        //UPPER CAMEL CASE
+        String FullName  = " Yeison Andres Parra Cordoba";
+        int SizeInCentimetersc = 37;
+
+
+        //LOWER CAMEL CASE
+        String fullName  = " Yeison Andres Parra Cordoba";
+        int sizeInCentimeters = 37;
+
+        //KEYWORD es = `final`
+
+
+    }
+}
